04ac0e66b40cfa79099d6b724e4f068135625aa5
bryanhancco <bhanccoco@unsa.edu.pe>
2022-05-06 11:34:47 -0500

    Agregando metodo con 1 parametro

---
+++ lab01/MethodParameters.java
@@ -0,0 +1,11 @@
+//Problema de ejemplo para el tema de metodos con parametros
+
+public class MethodParameters {
+	public static void main(String[] args) {
+		Hola("Bryan");
+	}
+	//Los metodos permiten parametros con una variable
+	public static void Hola(String nombre) {
+		System.out.println("Hola "+nombre+" bienvenido a la programacion en Java");
+	}
+}
