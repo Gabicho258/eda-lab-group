69d06a797d7f1a87ef1134df69c123d3f5de4d88
bryanhancco <bhanccoco@unsa.edu.pe>
2022-05-06 11:45:41 -0500

    Agregando metodo que devuelve valores con condicionales

--- lab01/MethodParameters.java
+++ lab01/MethodParameters.java
@@ -4,6 +4,11 @@ public class MethodParameters {
 	public static void main(String[] args) {
 		hola("Bryan");
 		registro("Bryan Orlando", "Hancco Condori", 20210571);
+		if (eresAdulto(19))
+			System.out.println("Eres mayor de edad");
+		else
+			System.out.println("NO Eres mayor de edad");
+		
 	}
 	//Los metodos permiten 1 parametro
 	public static void hola(String nombre) {
@@ -12,5 +17,11 @@ public class MethodParameters {
 	//Los metodos tambien permiten multiples parametros
 	public static void registro(String nombres, String apellidos, int CUI) {
 		System.out.println(apellidos+" / "+nombres+" - "+CUI);
+	}
+	//Los metodos tambien pueden devolver valores
+	public static boolean eresAdulto(int edad) {
+		if (edad >= 18)
+			return true;
+		return false;
 	}
 }
