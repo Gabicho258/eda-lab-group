0f88093509684d82256aaa72ea5d9de02e9a27d9
bryanhancco <bhanccoco@unsa.edu.pe>
2022-05-06 11:53:02 -0500

    Metodos sobrecargados para la division

--- lab01/MethodOverloading.java
+++ lab01/MethodOverloading.java
@@ -1,6 +1,17 @@
 //Problema de ejemplo para el tema de sobrecarga de metodos
 public class MethodOverloading {
 	public static void main(String[] args) {
-
+		int divEntera = division(91, 7);
+		double divReal = division(13.0, 7.0);
+		System.out.println("Resultado entero: "+divEntera);
+		System.out.println("Resultado decimal: "+divReal);
+	}
+	//Los metodos en Java admiten sobrecarga, con varios metodos
+	//con el MISMO NOMBRE
+	public static int division(int a, int b) {
+		return a/b;
+	}
+	public static double division(double a, double b) {
+		return a/b;
 	}
 }
