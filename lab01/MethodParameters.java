8a77fb524cf85daa67e1a920b60054f591f411db
bryanhancco <bhanccoco@unsa.edu.pe>
2022-05-06 11:40:35 -0500

    Metodo de multiples parametros

--- lab01/MethodParameters.java
+++ lab01/MethodParameters.java
@@ -2,10 +2,15 @@
 
 public class MethodParameters {
 	public static void main(String[] args) {
-		Hola("Bryan");
+		hola("Bryan");
+		registro("Bryan Orlando", "Hancco Condori", 20210571);
 	}
-	//Los metodos permiten parametros con una variable
-	public static void Hola(String nombre) {
+	//Los metodos permiten 1 parametro
+	public static void hola(String nombre) {
 		System.out.println("Hola "+nombre+" bienvenido a la programacion en Java");
+	}
+	//Los metodos tambien permiten multiples parametros
+	public static void registro(String nombres, String apellidos, int CUI) {
+		System.out.println(apellidos+" / "+nombres+" - "+CUI);
 	}
 }
