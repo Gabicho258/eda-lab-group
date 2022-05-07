dd689a842fa600e9115f4b56bd15ed5221246ff4
franco <franco@debian.fluchi>
2022-05-07 16:50:45 -0500

    agrego problema de comentarios en el cual aplico comentarios en linea y multilinea

---
+++ lab01/Comentarios.java
@@ -0,0 +1,13 @@
+/* Escriba un Hola mundo , y expliquelo usando comentarios en linea 
+y comentarios multilinea */
+public class Comentarios {
+
+    public static void main(String[] args) {
+
+        System.out.println("hola mundo");// esta sentencia imprimira Hola mundo en la consola
+        /*
+         * esta sentencia imprimira Hola mundo en
+         * la consola
+         */
+    }
+}
