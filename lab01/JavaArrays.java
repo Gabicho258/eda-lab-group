b76b03e57d32581f6c971f39f21399c34ed19d71
franco <franco@debian.fluchi>
2022-05-07 18:59:35 -0500

    declaro varialbes a usar y mensajes , coloco for para darle valor aleatorio a cada elemento de la matriz e imprimirlos

---
+++ lab01/JavaArrays.java
@@ -0,0 +1,26 @@
+/*Cree un programa que Genere e imprima una matriz de enteros (1-9) aleatoria de mxn (longitud ingresada por usuario) 
+y posteriormente sume sus elementos*/
+
+import java.util.*;
+
+public class JavaArrays {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Ingrese m y luego n (enteros)");
+        int m = sc.nextInt();
+        int n = sc.nextInt();
+        int matriz[][] = new int[m][n];
+        int suma = 0;
+        System.out.print("Matriz\n");
+        for (int i = 0; i < matriz.length; i++) {
+            for (int j = 0; j < matriz[i].length; j++) {
+                matriz[i][j] = (int) (Math.random() * 9) + 1;
+                System.out.print(matriz[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+    }
+}
