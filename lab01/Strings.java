712accf59e4503c5bb535b760f16d4ed3620dcb9
Carlo Rodrigo Diaz Portilla <cdiazpor@unsa.edu.pe>
2022-05-08 13:18:15 -0500

    Agrego el problema sobre strings

--- lab01/Strings.java
+++ lab01/Strings.java
@@ -1,4 +1,8 @@
-//Plantilla ejercicio Strings
+/*
+ * Crea un función que reciba un string (nombre), lo ponga en 
+ * mayúscula, luego concatenarlo para que imprima un saludo
+ * (Bienvenido PEDRO!) y la longitud del nombre (Longitud: 5)
+*/
 
 public class Strings {
 	public static void main (String [] args ) {
