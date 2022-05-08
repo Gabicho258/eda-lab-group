e2e7eca1ddfc14dcc976ee4d1a58cef535065454
Carlo Rodrigo Diaz Portilla <cdiazpor@unsa.edu.pe>
2022-05-08 12:00:37 -0500

    Agrego el ejercicio de Type Casting

--- lab01/TypeCasting.java
+++ lab01/TypeCasting.java
@@ -1,4 +1,7 @@
-//Plantilla ejercicio TypeCasting
+/*
+ * Escriba una función que reciba un double, haga un casting al valor
+ * y lo devuelva como un entero
+*/
 
 public class TypeCasting {
 	public static void main (String [] args) {
