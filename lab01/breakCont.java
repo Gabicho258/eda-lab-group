ad512d8cdec102eb6f2960ea4c852e71b88ea728
Barbara280801 <bcarrillo@unsa.edu.pe>
2022-05-08 17:28:06 -0500

    modificacion de ejercicio

--- lab01/breakCont.java
+++ lab01/breakCont.java
@@ -1,9 +1,12 @@
 import java.util.*;
-
+//method continue: Se tiene un array de nùmeros los cuales se imprimiràn excepto los mùltiplos de 5
 public class breakCont {
   public static void main(String[] args){
-    System.out.println("Hola munda");
 
+    for(int i=0;i<=22;i++){
+      if(i/5==0)continue;
+      System.out.println(i);
+    }
   }
 }
 
