db6f0ab9c3a55592b3addb990ea9052b5a4e8ce4
Barbara280801 <bcarrillo@unsa.edu.pe>
2022-05-08 13:40:56 -0500

    añado el ejemplo de for loop con un problema

--- lab01/forloop.java
+++ lab01/forloop.java
@@ -1,8 +1,15 @@
 import java.util.*;
 
+//Se quiere tener una lista enumerada de un conjunto de personas.
+
 public class forloop {
   public static void main(String[] args){
-    System.out.println("Hola munda");
+    String names[] = {"Ana","Brandon","Carlos","Daymar","Emanuel","Fabiola","Josue","Daniel","Ismael"}; 
+
+    //for loop utilizado
+    for(int i=0;i<names.length;i++){
+      System.out.println((i+1)+"."+names[i]);
+    }
 
   }
 }
