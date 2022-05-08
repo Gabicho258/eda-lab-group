f09dcf3e8ad99a5194bc9ab099dbdcfc7fd87509
Barbara280801 <bcarrillo@unsa.edu.pe>
2022-05-08 17:42:51 -0500

    agrego un break para complementa el ejercicio

--- lab01/breakCont.java
+++ lab01/breakCont.java
@@ -1,10 +1,12 @@
 import java.util.*;
-//method continue: Se tiene un array de nùmeros los cuales se imprimiràn excepto los mùltiplos de 5
+//method continue: Se tienen nùmeros del 0 al 22 los cuales se imprimiràn excepto los mùltiplos de 5 y los nùmeros impresos deberàn ser menores a 16
+//
 public class breakCont {
   public static void main(String[] args){
     int i = 0;
 
     while(i<=22){
+      if(i==16)break;
       if((i%5)==0){
 	i++;
 	continue;
