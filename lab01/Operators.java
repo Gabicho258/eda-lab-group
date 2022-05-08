4843472b3c790779827b4370d13740d9551ce27c
Carlo Rodrigo Diaz Portilla <cdiazpor@unsa.edu.pe>
2022-05-08 12:24:13 -0500

    Agrego el problema sobre operadores

--- lab01/Operators.java
+++ lab01/Operators.java
@@ -1,4 +1,8 @@
-//Plantilla ejercicio Operators
+/*
+ * Cree una calculadora simple con las operaciones básicas
+ * adición (+), resta (-), multiplicación (*), división (/)
+ * y módulo (%)
+*/
 
 public class Operators {
 	public static void main (String [] args) {
