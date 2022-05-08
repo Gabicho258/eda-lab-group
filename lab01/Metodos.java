e34991626c990fc93eb87c9c42c83ec1a3a3c486
franco <franco@debian.fluchi>
2022-05-07 20:47:09 -0500

    agrego metodo primos en un rango que evalua solo impares ya que el unico primo par es el 2

--- lab01/Metodos.java
+++ lab01/Metodos.java
@@ -2,7 +2,7 @@
 
 public class Metodos {
     public static void main(String[] args) {
-
+        primosRango(103, 103);
     }
 
     static boolean esPrimo(int n) {
@@ -19,6 +19,15 @@ public class Metodos {
     }
 
     static void primosRango(int a, int b) {
+        if (a <= 2 && b >= 2)
+            System.out.print(2 + " ");
+        // como todo ningun par es primo uso operadores ternarios para iniciar por un
+        // primo y luego incremento de 2 en 2
+        for (int i = (a % 2 == 0) ? a + 1 : a; i <= b; i += 2) {
+            if (esPrimo(i)) {
+                System.out.print(i + " ");
+            }
+        }
 
     }
 }
