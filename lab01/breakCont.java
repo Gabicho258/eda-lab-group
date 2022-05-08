044386af0228c0f00706c63b93cc0a4a93b0a7e4
Barbara280801 <bcarrillo@unsa.edu.pe>
2022-05-08 17:38:01 -0500

    modifico el codigo usando un while

--- lab01/breakCont.java
+++ lab01/breakCont.java
@@ -2,10 +2,15 @@ import java.util.*;
 //method continue: Se tiene un array de nùmeros los cuales se imprimiràn excepto los mùltiplos de 5
 public class breakCont {
   public static void main(String[] args){
+    int i = 0;
 
-    for(int i=0;i<=22;i++){
-      if(i/5==0)continue;
+    while(i<=22){
+      if((i%5)==0){
+	i++;
+	continue;
+      }
       System.out.println(i);
+      i++;
     }
   }
 }
