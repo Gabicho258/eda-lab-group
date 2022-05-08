94eb82ff231554cc82e617ff08215ed89aff806d
Barbara280801 <bcarrillo@unsa.edu.pe>
2022-05-08 14:12:36 -0500

    agrego funciòn for each para complementar los subtipos del bucle, modifico el problema inicial

--- lab01/forloop.java
+++ lab01/forloop.java
@@ -1,16 +1,23 @@
 import java.util.*;
 
-//Se quiere tener una lista enumerada de un conjunto de personas.
+//Se quiere tener una lista enumerada de un conjunto de personas(listas separadas segùn el sexo).
 
 public class forloop {
   public static void main(String[] args){
-    String names[] = {"Ana","Brandon","Carlos","Daymar","Emanuel","Fabiola","Josue","Daniel","Ismael"}; 
+    String nameH[] = {"Brandon","Carlos","Daymar","Emanuel","Flavio","Josue","Daniel","Ismael"}; 
+    String nameM[] = {"Ana","Barbie","Carmen","Ema","Nadia","Maria","Isabella"}; 
 
     //for loop utilizado
-    for(int i=0;i<names.length;i++){
-      System.out.println((i+1)+"."+names[i]);
+    for(int i=0;i<nameH.length;i++){
+      System.out.println((i+1)+"."+nameH[i]);
     }
 
+    //for each utilizado
+    int ind=1;
+    for(String i:nameM){
+      System.out.println((ind)+"."+i);
+      ind++;
+    }
   }
 }
 
